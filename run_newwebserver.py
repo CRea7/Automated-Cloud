0a95540d6dd66f32823c640f9ca95b1f0a24bf2f
CRea7 <conorrea123@gmail.com>
2019-03-04 14:55:08 +0000

    group not being passed into run

--- run_newwebserver.py
+++ run_newwebserver.py
@@ -1,51 +1,55 @@
 #!/usr/bin/env python3
 import boto3
 from botocore.exceptions import ClientError
-#from consolemenu import
-#from consolemenu.items import
 
-#menu = ConsoleMenu("Assignment automation", "Subtitle")
+def main():
+    security_group_id = create_security()
+    create_instance(security_group_id)
 
-#menu_item = MenuItem("Menu Item")
+def create_security():
 
-#function_item = FunctionItem(create())
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_vpcs()
+    vpc_id = 'vpc-05ec96ec41ae536da'
 
-#def create():
-#create a security group
-ec2 = boto3.client('ec2')
+    try:
+        response = ec2.create_security_group(GroupName='Assignment Group',
+                                            Description='used for assignment',
+                                            VpcId=vpc_id)
+        security_group_id = response['GroupId']
+        print('Security Group Created :' + str(security_group_id))
 
-response = ec2.describe_vpcs()
-vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+        data = ec2.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions=[
+                {'IpProtocol': 'tcp',
+                'FromPort': 80,
+                'ToPort': 80,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                {'IpProtocol': 'tcp',
+                'FromPort': 22,
+                'ToPort': 22,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+            ])
+    #print('Ingress Successfully Set %s' % data)
+    except ClientError as e:
+        print(e)
+    return security_group_id
 
-try:
-    response = ec2.create_security_group(GroupName='Assignment Group',
-                                         Description='used for assignment',
-                                         VpcId=vpc_id)
-    security_group_id = response['GroupId']
-    print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+#creates instance using security group that was previously created and my key pair
+#KeyPair = CRea_KeyPair.pem
+def create_instance(security_group_id):
 
-    data = ec2.authorize_security_group_ingress(
-        GroupId=security_group_id,
-        IpPermissions=[
-            {'IpProtocol': 'tcp',
-             'FromPort': 80,
-             'ToPort': 80,
-             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-            {'IpProtocol': 'tcp',
-             'FromPort': 22,
-             'ToPort': 22,
-             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-        ])
-    print('Ingress Successfully Set %s' % data)
-except ClientError as e:
-    print(e)
+    ec2 = boto3.resource('ec2')
+    instance = ec2.create_instances(
+        ImageId='ami-047bb4163c506cd98',
+        MinCount=1,
+        MaxCount=1,
+        SecurityGroupIds=[security_group_id],
+        #KeyName = KeyPair,   #key pair located in dcuments
+        InstanceType='t2.micro')
+    print (instance[0].id)
 
 
-ec2 = boto3.resource('ec2')
-instance = ec2.create_instances(
-    ImageId='ami-047bb4163c506cd98',
-    MinCount=1,
-    MaxCount=1,
-    SecurityGroupIds=[security_group_id],
-    InstanceType='t2.micro')
-print (instance[0].id)
+if __name__ == '__main__':
+    main()
